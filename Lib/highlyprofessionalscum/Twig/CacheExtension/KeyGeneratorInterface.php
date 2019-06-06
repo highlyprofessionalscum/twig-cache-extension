3c825eba79955abf0dc71c2cdd4f63973d9b185d
stayeronglass <stayeronglass@gmail.com>
2019-06-06 14:04:27 +0300

    123

---
+++ Lib/highlyprofessionalscum/Twig/CacheExtension/KeyGeneratorInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace highlyprofessionalscum\Twig\CacheExtension;
+
+interface KeyGeneratorInterface
+{
+/**
+* Generate a cache key for a given value.
+*
+* @param mixed $value
+*
+* @return string
+*/
+public function generateKey($value);
+
+}
